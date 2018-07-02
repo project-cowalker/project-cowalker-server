d83c4d30cfa63c8ce54dfe349f7bdcea732b7638
daseal bae <ektmf1993@gmail.com>
2018-07-03 01:28:58 +0900

    add signin

--- src/app/controller/user/user_routes.js
+++ src/app/controller/user/user_routes.js
@@ -5,4 +5,8 @@ const router = express.Router();
 const signup = require('./signup');
 router.use('/signup', signup);
 
+//signup
+const signin = require('./signin');
+router.use('/signin', signin);
+
 module.exports = router;
