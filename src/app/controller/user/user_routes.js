15286f67cee59e543c2fee835395c915073400c9
daseal bae <ektmf1993@gmail.com>
2018-07-02 21:54:24 +0900

    edit pool

--- src/app/controller/user/user_routes.js
+++ src/app/controller/user/user_routes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-//user
-const user = require('./user/user_routes');
-router.use('/', user);
+
 
 module.exports = router;
--- src/app/module/pool.js
+++ src/app/module/pool.js
@@ -10,8 +10,9 @@ module.exports = {
     execute1 : async (...args) => {
         const query = args[0];
         let result;
+        let connection;
         try {
-            let connection = await pool.getConnection();
+            connection = await pool.getConnection();
             result = await connection.query(query) || null;
         }
         catch(err) {
@@ -27,8 +28,9 @@ module.exports = {
         const query = args[0];
         const data = args[1];
         let result;
+        let connection;
         try {
-            let connection = await pool.getConnection();
+            connection = await pool.getConnection();
             result = await connection.query(query, data) || null;
         }
         catch(err) {
@@ -45,8 +47,9 @@ module.exports = {
         const data = args[1];
         const data2 = args[2];
         let result;
+        let connection;
         try {
-            let connection = await pool.getConnection();
+            connection = await pool.getConnection();
             result = await connection.query(query, [data, data2]) || null;
         }
         catch(err) {
@@ -64,8 +67,9 @@ module.exports = {
         const data2 = args[2];
         const data3 = args[3];
         let result;
+        let connection;
         try {
-            let connection = await pool.getConnection();
+            connection = await pool.getConnection();
             result = await connection.query(query, [data, data2, data3]) || null;
         }
         catch(err) {
