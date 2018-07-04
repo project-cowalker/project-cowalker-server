78f26025579d65efe176630a8da68eea460ce83b
Kyuhee Lim <kyuhee2118@gmail.com>
2018-07-04 22:22:36 +0900

    modifing

--- src/app/controller/recruit/recruit_routes.js
+++ src/app/controller/recruit/recruit_routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 
-///recruit/create : 팀원 모집
+//모집
 const create = require('./recruit_create');
 router.use('/recruit', create);
+
+// 
+const getRecruit=require('./getRecruit');
+router.use('/recruit',getRecruit);
 
 module.exports = router;
