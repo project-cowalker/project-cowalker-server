4388d37fc51f501705386f3a524bf13bb8c9c4cd
daseal bae <ektmf1993@gmail.com>
2018-07-06 00:19:32 +0900

    edit delete message

--- src/app/controller/message/postMessage.js
+++ src/app/controller/message/postMessage.js
@@ -5,9 +5,9 @@ const message = require('../../model/schema/message');
 
 router.post('/:to_idx', async (req, res, next) => {
     const ID = jwt.verify(req.headers.authorization);
-    console.log(ID);
-    console.log(req.params.to_idx);
+
     if (ID != -1) {
+
         message.create({
             to_idx : req.params.to_idx,
             from_idx : ID,
@@ -17,25 +17,23 @@ router.post('/:to_idx', async (req, res, next) => {
                 return res.status(500).send({
                     message: 'send message fail'
                 });
-            }else {
-                console.log("second");
-                message.create({
-                    to_idx : ID,
-                    from_idx : req.params.to_idx,
-                    contents : req.body.contents
-                }), await function(err, data) {
-                    console.log(data);
-                    if(err) {
-                        return res.status(500).send({
-                            message: 'send message fail'
-                        });
-                    }else {
-                        return res.status(201).send({
-                            message: 'send message success'
-                        });
-                    }
-                }
             }
+        });
+
+        message.create({
+            from_idx : req.params.to_idx,
+            to_idx : ID,
+            contents : req.body.contents
+        }, function(err, obj){
+            if(err){
+                return res.status(500).send({
+                    message: 'send message fail'
+                });
+            }
+        });
+
+        return res.status(201).send({
+            message: 'send message success'
         });
 
     }else {
