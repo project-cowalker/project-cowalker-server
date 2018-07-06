5c94ff496bba0bf4d3e8e417b7f46f3e65a606f3
daseal bae <ektmf1993@gmail.com>
2018-07-07 01:41:58 +0900

    working search

--- src/app/routes.js
+++ src/app/routes.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 const user = require('./controller/user/user_routes');
 router.use('/', user);
 
@@ -19,5 +18,9 @@ router.use('/', message);
 //project
 const project = require('./controller/project/project_routes');
 router.use('/', project);
+
+//project
+const search = require('./controller/search/search_routes');
+router.use('/', search);
 
 module.exports = router;
