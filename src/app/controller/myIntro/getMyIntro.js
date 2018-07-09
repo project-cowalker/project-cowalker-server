5822cba21b7cc7bb8c303420ced6632cb05cda94
bghgu <dsbae@smilegate.com>
2018-07-09 11:00:22 +0900

    edit myIntro req params

--- src/app/controller/myIntro/getMyIntro.js
+++ src/app/controller/myIntro/getMyIntro.js
@@ -4,43 +4,34 @@ const jwt = require('../../module/jwt.js');
 const db = require('../../module/pool.js');
 const myIntro = require('../../model/schema/myIntro');
 
-router.get('/', async (req, res, next) => {
-    const ID = jwt.verify(req.headers.authorization);
+router.get('/:user_idx', async (req, res, next) => {
     let data = new Array();
 
-    if (ID != -1) {
-        myIntro.find({user_idx : ID}, function (err, result) {
-            if(err) {
-                return res.status(405).send({
-                    message: 'get message fail'
-                });
-            }else {
-                for(i = 0; i < result.length; i++) {
-                    let temp = {
-                        intro_idx : "",
-                        intro_contents : "",
-                        intro_img_url : ""
-                    }
-                    temp.intro_idx = result[i]._id;
-                    temp.intro_contents = result[i].intro_contents;
-                    temp.intro_img_url = result[i].intro_img_url;
-                    data.push(temp);
+    myIntro.find({user_idx : user_idx}, function (err, result) {
+        if(err) {
+            return res.status(405).send({
+                message: 'get message fail'
+            });
+        }else {
+            for(i = 0; i < result.length; i++) {
+                let temp = {
+                    intro_idx : "",
+                    intro_contents : "",
+                    intro_img_url : ""
                 }
-                
-                res.status(200).send({
-                    message: "success",
-                    result: data,
-                });
-                return;
+                temp.intro_idx = result[i]._id;
+                temp.intro_contents = result[i].intro_contents;
+                temp.intro_img_url = result[i].intro_img_url;
+                data.push(temp);
             }
-        });
-    }
-    else {
-        res.status(401).send({
-            message: "access denied"
-        });
-    }
-
+            
+            res.status(200).send({
+                message: "success",
+                result: data,
+            });
+            return;
+        }
+    });
 });
 
 module.exports = router;
