68ef1aa43e82c149e9cd7828aca419ee7908bf7f
mmyungji <hallomj1@gmail.com>
2018-07-11 21:23:03 +0900

    edit myIntroRes

--- src/app/model/res/introRes.js
+++ src/app/model/res/introRes.js
@@ -1,11 +1,14 @@
 module.exports = {
     res : function(result) {
         let temp = {
+        	intro_idx : "",
             intro_contents : "",
             intro_img_url : ""
         }
         temp.intro_contents = result[0].intro_contents;
         temp.intro_img_url = result[0].intro_img_url;
+        temp.intro_idx = result[0]._id;
+
         return temp;
     }   
 }
