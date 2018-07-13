b329ba8557f5f656a4dcaf5ea3a6cc8d115546fe
mmyungji <hallomj1@gmail.com>
2018-07-13 21:42:07 +0900

    edit message time

--- src/app/controller/message/getMessage.js
+++ src/app/controller/message/getMessage.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const jwt = require('../../module/jwt.js');
 const db = require('../../module/pool.js');
 const message = require('../../model/schema/message');
+const time = require('../../module/time');
 
 router.get('/:partner_idx', async (req, res, next) => {
     const ID = jwt.verify(req.headers.authorization);
@@ -35,7 +36,8 @@ router.get('/:partner_idx', async (req, res, next) => {
                         to_user_name : "",
                         from_user_name : "",
                         contents : "",
-                        create_at : ""
+                        create_at : "",
+                        time : ""
                     }
                     if(obj[i].to_idx == ID) {
                         temp.to_user_name = "나";
@@ -46,6 +48,7 @@ router.get('/:partner_idx', async (req, res, next) => {
                     }
                     temp.contents = obj[i].contents;
                     temp.create_at = obj[i].create_at;
+                    temp.time = time.elapsedTime(obj[i].create_at);
                     data.push(temp);
                 }
                 res.status(200).send({
